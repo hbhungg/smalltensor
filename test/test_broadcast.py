96109b540fd7c00c70c2cc5c320f838347c10149
hung <bahung1tt@gmail.com>
2023-02-09 17:22:16 +1100

    Fix backward bug on broadcast ops.
    * Tensor.ensure_tensor -> Tensor.broadcasted_tensor
    
    * Manually do broadcast using expand ops, so that backward can use the
      ops to do backward.
    
    * Backward now return the correct shape on broadcasted Tensor.
    
    * Add some helpers functions to figures out the broadcasted shapes and
      broadcasted indices.
    
    * Add some helper method to construct tensor (zeros, ones, randn).
    
    * Fix Expand.backward bug, now correctly passing the axes that would
      reduce to the correct shape before expanding.
    
    * Add some tests for the broadcast helper functions.
    
    * Add test to check if the new backward can deal with broadcast Tensor.

--- test/test_broadcast.py
+++ test/test_broadcast.py
@@ -1,6 +1,6 @@
 import pytest
 
-from smalltensor.utils import broadcast_shapes
+from smalltensor.utils import broadcast_shapes, broadcast_indices
 
 def test_broadcast_shapes_equal():
   assert broadcast_shapes((1, 2), (3, 2)) == (3, 2)
@@ -13,3 +13,21 @@ def test_broadcast_shapes_unequal():
 def test_broascast_shapes_fail():
   with pytest.raises(ValueError):
     broadcast_shapes((3, 4), (5,))
+
+def test_broadcast_indices_equal():
+  assert broadcast_indices((1, 2), (3, 2)) == (0,)
+  assert broadcast_indices((1, 2), (3, 1)) == (0, 1)
+
+def test_broadcast_indices_unequal():
+  assert broadcast_indices((7,), (5, 1, 7)) == (0, 1)
+  assert broadcast_indices((6, 7), (5, 6, 1)) == (0, 2)
+
+import numpy as np
+from smalltensor import Tensor
+def test_broadcast_backward():
+  a = Tensor.randn(1, 3, requires_grad=True)
+  b = Tensor.randn(3, 3, requires_grad=True)
+  c = a + b
+  c.backward()
+  assert a.grad.eq(Tensor.ones(1, 3) * 3)
+  assert b.grad.eq(Tensor.ones(3, 3))
