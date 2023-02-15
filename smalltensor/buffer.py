d6cc8ffe9959f6680df4ee2210bf4b7ec551a5c6
hung <bahung1tt@gmail.com>
2023-02-15 20:37:48 +1100

    flat struture, change the setup python version req

--- smalltensor/buffer.py
+++ smalltensor/buffer.py
@@ -1,14 +1,4 @@
-from typing import Optional, Tuple, Union, List, Dict
 import numpy as np
 
-class Buffer:
-  def __init__(self, buffer: Union[np.ndarray, List]):
-    self._buffer = buffer if isinstance(buffer, np.ndarray) else np.array(buffer)
-
-  @property
-  def shape(self):
-    return self._buffer.shape
-
-  @property
-  def strides(self):
-    return self._buffer.strides
+class Buffer(np.ndarray):
+  pass
