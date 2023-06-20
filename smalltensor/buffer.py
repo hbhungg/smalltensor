3cfd60bde4b979ad628401d53e8f01a40597c410
hung <bahung1tt@gmail.com>
2023-06-20 17:35:33 +1000

    numpy buffer

--- smalltensor/buffer.py
+++ smalltensor/buffer.py
@@ -1,4 +1,73 @@
+from __future__ import annotations
 import numpy as np
+from enum import Enum, auto
+from typing import Union
 
-class Buffer(np.ndarray):
-  pass
+class UnaryOps(Enum):
+  NEG = auto()
+  EXP = auto()
+  LOG = auto()
+
+class BinaryOps(Enum): 
+  ADD = auto()
+  SUB = auto()
+  MUL = auto()
+  DIV = auto()
+  POW = auto()
+  CMPEQ = auto()
+
+class ReduceOps(Enum):
+  SUM = auto()
+  MAX = auto() # max(a) = a[0]; max element
+
+class MovementOps(Enum): 
+  RESHAPE = auto()
+  PERMUTE = auto()
+  EXPAND = auto()
+  # PAD = auto()
+  # SHRINK = auto()
+  # STRIDE = auto()
+
+Op = Union[UnaryOps, BinaryOps, ReduceOps, MovementOps]
+
+class Buffer:
+  def unary_op(self, op: BinaryOps):
+    raise NotImplemented(f"Not implemented for {self}")
+
+  def binary_op(self, op: BinaryOps, val):
+    raise NotImplemented(f"Not implemented for {self}")
+
+  def reduce_op(self, op: ReduceOps, shape, keepdims):
+    raise NotImplemented(f"Not implemented for {self}")
+
+  def movement_op(self, op: MovementOps, shape):
+    raise NotImplemented(f"Not implemented for {self}")
+
+
+class NumpyBuffer(Buffer, np.ndarray):
+  def unary_op(self, op: UnaryOps):
+    match op:
+      case UnaryOps.NEG: return np.negative(self)
+      case UnaryOps.EXP: return np.exp(self)
+      case UnaryOps.LOG: return np.log(self)
+
+  def binary_op(self, op: BinaryOps, val: NumpyBuffer):
+    match op:
+      case BinaryOps.ADD: return np.add(self, val)
+      case BinaryOps.SUB: return np.subtract(self, val)
+      case BinaryOps.MUL: return np.multiply(self, val)
+      case BinaryOps.POW: return np.power(self, val)
+      case BinaryOps.CMPEQ: return np.array_equal(self, val)
+  
+  def reduce_op(self, op: ReduceOps, shape, keepdims):
+    match op:
+      case ReduceOps.SUM: return np.sum(self, shape, keepdims=keepdims)
+      case ReduceOps.MAX: return np.amax(self, shape, keepdims=keepdims)
+
+  def movement_op(self, op: MovementOps, arg):
+     match op:
+      case MovementOps.RESHAPE: return np.reshape(self, arg)
+      case MovementOps.PERMUTE: return np.transpose(self, arg)
+      case MovementOps.EXPAND: return np.broadcast_to(self, arg)
+
+
---
+++ test/test_buffer.py
@@ -0,0 +1,15 @@
+import numpy as np
+from smalltensor.buffer import NumpyBuffer
+from smalltensor.buffer import UnaryOps
+
+# Right now, most of this test feels redundant since we are testing numpy backend using numpy
+def test_unary_op():
+  data = np.random.rand(2, 2)
+  a = NumpyBuffer((2,2), buffer=data)
+  assert np.allclose(a.unary_op(UnaryOps.NEG), -data)
+  assert np.allclose(a.unary_op(UnaryOps.EXP), np.exp(data))
+
+def test_binary_op():
+  d1 = np.random.rand(2, 2)
+  d2 = np.random.rand(2, 2)
+  assert np.allclose(NumpyBuffer((2, 2), buffer=d1)+NumpyBuffer((2, 2), buffer=d2), d1+d2)
